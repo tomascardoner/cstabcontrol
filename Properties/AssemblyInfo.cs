49e9298c17133c07a1fc15344bc1a17c01f241af
Tomás A. Cardoner <tomas@cardoner.com.ar>
2022-05-01 21:49:58 -0300

    NET Framework update

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CS-Tab")]
+[assembly: AssemblyTitle("CS-Tab Control")]
 [assembly: AssemblyDescription("Tab Component")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Cardoner Sistemas")]
-[assembly: AssemblyProduct("CS-Tab")]
-[assembly: AssemblyCopyright("Copyright © 2019-2020 Cardoner Sistemas")]
-[assembly: AssemblyTrademark("® CS-Tab")]
+[assembly: AssemblyProduct("CS-Tab Control")]
+[assembly: AssemblyCopyright("Copyright © 2019-2022 Cardoner Sistemas")]
+[assembly: AssemblyTrademark("® CS-Tab Control")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
